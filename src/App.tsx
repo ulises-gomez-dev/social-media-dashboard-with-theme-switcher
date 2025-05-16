9aa0c8a0b620894dba3f129f3c2b81af6f14be6f
Ulises Gomez <usg@arizona.edu>
2025-05-15 22:59:25 -0700

    header, line and theme toggle added to App component; added dark mode functionality; applied minor styles and flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [theme, setTheme] = useState("light");
+
+  document.documentElement.setAttribute("data-theme", theme);
+
+  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+
+  return (
+    <div className="dashboard">
+      <div className="top-half-bg"></div>
+      <header className="dashboard-title">
+        <h1>Social Media Dashboard</h1>
+        <p>Total Followers: 23,004</p>
+      </header>
+      <div className="line"></div>
+      <div className="theme-toggle">
+        <p>Dark Mode</p>
+        {/* button tag is temporary */}
+        <button onClick={toggleTheme}>toggle</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
